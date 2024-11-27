c376d6a5d0ffee0c92bd7030ebbb2c9ef70d6c5a
Ammar Abdelhalem <ammarabdelhalem19@gmail.com>
2024-11-27 13:18:08 +0200

    add get_all_hr_info endpoint

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-from sqlmodel import create_engine, Session, SQLModel
-from fastapi import FastAPI
+from sqlmodel import create_engine, Session, SQLModel, select
+from fastapi import FastAPI, Depends
+from app.database.model import Hr
+from app.models.hr import HrQ
 from dotenv import load_dotenv
 import os
 
@@ -17,3 +19,9 @@ def get_session():
 @app.on_event(event_type="startup")
 def on_startup():
     SQLModel.metadata.create_all(engine)
+
+@app.get("/api/hr", tags=["Hr"])
+def get_all_hr_info(session: Session = Depends(get_session)):
+    statment = select(Hr)
+    result = session.exec(statment).all()
+    return {"message": result}
